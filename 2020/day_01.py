eb9dc8515657a6ca754e5f582a51ee5a77142712
Lucas Black <hmnotsocool@gmail.com>
2020-12-02 01:51:27 -0800

    first day

---
+++ 2020/day_01.py
@@ -0,0 +1,30 @@
+print('PART ONE')
+print('--------')
+
+nums = []
+with open("day_01_input", "r") as f:
+	for line in f:
+		nums.append(int(line))
+
+nums.sort()
+
+for x in range(0, len(nums)):
+	for y in range(len(nums) - 1, x, -1):
+		if nums[x] + nums[y] == 2020:
+			print("found " + str(nums[x]) + " + " + str(nums[y]) + " = " + "2020")
+			print("solution is " + str(nums[x] * nums[y]))
+
+print('\n')
+print('PART TWO')
+print('--------')
+
+nums = [x for x in nums if x < 1250]
+
+for x in range(0, len(nums)):
+	for y in range(len(nums) - 1, x, -1):
+		for z in range(x, y + 1):
+			if nums[x] + nums[y] + nums[z] == 2020:
+				print("found " + str(nums[x]) + " + " + str(nums[y]) \
+					+ " + " + str(nums[z]) + " = " + "2020")
+				print("solution is " + str(nums[x] * nums[y] * nums[z]))
+
