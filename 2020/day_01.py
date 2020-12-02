1134a5768044ef9f24032fc337e17cb8ee400c05
Lucas Black <hmnotsocool@gmail.com>
2020-12-02 01:52:36 -0800

    tabs to spaces :3

--- 2020/day_01.py
+++ 2020/day_01.py
@@ -3,16 +3,16 @@ print('--------')
 
 nums = []
 with open("day_01_input", "r") as f:
-	for line in f:
-		nums.append(int(line))
+    for line in f:
+        nums.append(int(line))
 
 nums.sort()
 
 for x in range(0, len(nums)):
-	for y in range(len(nums) - 1, x, -1):
-		if nums[x] + nums[y] == 2020:
-			print("found " + str(nums[x]) + " + " + str(nums[y]) + " = " + "2020")
-			print("solution is " + str(nums[x] * nums[y]))
+    for y in range(len(nums) - 1, x, -1):
+        if nums[x] + nums[y] == 2020:
+            print("found " + str(nums[x]) + " + " + str(nums[y]) + " = " + "2020")
+            print("solution is " + str(nums[x] * nums[y]))
 
 print('\n')
 print('PART TWO')
@@ -21,10 +21,10 @@ print('--------')
 nums = [x for x in nums if x < 1250]
 
 for x in range(0, len(nums)):
-	for y in range(len(nums) - 1, x, -1):
-		for z in range(x, y + 1):
-			if nums[x] + nums[y] + nums[z] == 2020:
-				print("found " + str(nums[x]) + " + " + str(nums[y]) \
-					+ " + " + str(nums[z]) + " = " + "2020")
-				print("solution is " + str(nums[x] * nums[y] * nums[z]))
+    for y in range(len(nums) - 1, x, -1):
+        for z in range(x, y + 1):
+            if nums[x] + nums[y] + nums[z] == 2020:
+                print("found " + str(nums[x]) + " + " + str(nums[y]) \
+                    + " + " + str(nums[z]) + " = " + "2020")
+                print("solution is " + str(nums[x] * nums[y] * nums[z]))
 
